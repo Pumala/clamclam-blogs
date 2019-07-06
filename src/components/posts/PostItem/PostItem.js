05fd6232afe85440cef5c7cfea4c6251b02da592
Lyn <carolyn_lam1@homedepot.com>
2019-07-06 14:36:40 -0500

    clean up

--- src/components/posts/PostItem/PostItem.js
+++ src/components/posts/PostItem/PostItem.js
@@ -7,8 +7,6 @@ const PostItem = ({ post }) => {
 
     const { id, authorId, title, firstName, lastName, createdAt, lastUpdatedAt } = post;
 
-    // console.log('post in post item:', post);
-
     return (
         <div className="post">
             <h3><Link to={`/post/${id}`}>{title}</Link></h3>
